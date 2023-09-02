effbe4d1821931c687144ecaf378dea6b29462bf
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-09-02 23:53:25 +0300

    Commit 2

--- script.js
+++ script.js
@@ -1,80 +1,126 @@
-const board = document.getElementById('board');
-const log = document.getElementById('log');
+const board = document.getElementById("board");
+const log = document.getElementById("log");
 
 let arrayMain = [];
 
 function generateArray() {
-    for (let i = 0; i < 9; i++) {
-        let tempArray = [];
-        for (let j = 0; j < 9; j++) {
-
-            let tile = document.createElement("div");
-            let tempNumber = getRandomNumber();
-            tile.id ='';
-            tile.dataset.coords = i.toString()+'_'+j.toString();
-            tile.classList.add(`tile`);
-            tile.textContent = tempNumber;
-            tile.addEventListener("mousedown", handleMouseDown);
-            board.append(tile);
-            tempArray.push(tile);
-        }
-        arrayMain.push(tempArray);
+  for (let i = 0; i < 9; i++) {
+    let tempArray = [];
+    for (let j = 0; j < 9; j++) {
+      let tile = document.createElement("div");
+      let tempNumber = getRandomNumber();
+      tile.id = "";
+      tile.dataset.coords = i.toString() + "_" + j.toString();
+      tile.dataset.row = i;
+      tile.dataset.col = j;
+      tile.classList.add(`tile`);
+      tile.textContent = tempNumber;
+      tile.addEventListener("mousedown", handleMouseDown);
+      board.append(tile);
+      tempArray.push(tile);
     }
+    arrayMain.push(tempArray);
+  }
 }
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 6 + 1);
+  return Math.floor(Math.random() * 6 + 1);
 }
 
 generateArray();
 
 // events ----------------------------------------------------------------------
 
+let currTile;
 let tilesArray = [];
 let count = 0;
+let row1, col1;
+let score = 0;
 
 function handleMouseDown() {
-    currTile = this;
-
-    tilesArray.push(currTile);
+  currTile = this;
   
-    console.log(tilesArray.map(item => item.textContent));
+  currTile.removeEventListener("mousedown", handleMouseDown);
+  
+  tilesArray.push(currTile);
+  currTile.classList.add("checked");
+  
+  console.log(tilesArray.map((item) => item.textContent));
+  removeListeners();
 
-    if (tilesArray.length > 0) {
-        tilesArray[count].classList.add('checked');
-    }
-
-    if (tilesArray.length > 1) {
-
-        let summm = parseInt(tilesArray.reduce((accumulator, currentValue) => accumulator + currentValue, 1));
-        
-        if (parseInt(tilesArray[0].textContent) <= parseInt(tilesArray[1].textContent)) {
-
-            console.log('LOSE');
-            tilesArray[0].classList.remove('checked');
-            tilesArray[1].classList.remove('checked');
-            tilesArray = [];
-            count = 0;
-
-        } else if (parseInt(tilesArray[0].textContent) === summm) {
-            
-            console.log('win');
-
-            tilesArray.forEach(element => {
-                element.classList.remove('checked');
-            });
-                       
-            tilesArray = [];
-            count = 0;
-    
-        }
-    }
+  if (tilesArray.length > 1) {
     count++;
+    let sumArray = 0;
+    for (let k = 1; k < tilesArray.length; k++) {
+        sumArray += parseInt(tilesArray[k].textContent);
+    }
+    if (tilesArray[0].textContent <= tilesArray[1].textContent ||
+        tilesArray[0].textContent < sumArray) {
+      console.log("LOSE");
+      score -= parseInt(tilesArray[0].textContent);
+      addListeners();
+      checkWinLose (0);
+    } else if (tilesArray[0].textContent == sumArray) {
+      console.log("win");
+      score += parseInt(tilesArray[0].textContent);
+      addListeners();
+      checkWinLose (1);
+    }
+  }
 }
 
+function checkWinLose (index) {
+  tilesArray.forEach((element) => {
+    element.classList.remove("checked");
+      if (index == 0) {
+        
+        element.classList.add("red");
+      } else if (index == 1) {
+        
+        element.classList.add("blue");
+      }
+    element.removeEventListener("mousedown", handleMouseDown);
+  });
+  log.innerHTML = `Score: ${score}`;
+  tilesArray = [];
+  count = 0;
+}
 
-function checkCheck(currTile) {
-    let tileCoords = currTile.dataset.coords.split('_');
-    let row1 = parseInt(tileCoords[0]);
-    let col1 = parseInt(tileCoords[1]); 
+function addListeners() {
+  arrayMain.forEach(element => {
+    for (let i = 0; i < element.length; i++) {
+      element[i].addEventListener("mousedown", handleMouseDown);
+    }
+  });
+}
+
+function removeListeners() {
+  arrayMain.forEach(element => {
+    for (let i = 0; i < element.length; i++) {
+      element[i].removeEventListener("mousedown", handleMouseDown);
+    }
+  });
+
+  checkMove();
+  
+}
+
+function checkMove() {
+  let row = parseInt(tilesArray[0].dataset.row);
+  let col = parseInt(tilesArray[0].dataset.col);
+  
+  if (arrayMain[row] && arrayMain[row][col+1]) {
+    arrayMain[row][col+1].addEventListener("mousedown", handleMouseDown);
+  }
+  if (arrayMain[row] && arrayMain[row][col-1]) {
+    arrayMain[row][col-1].addEventListener("mousedown", handleMouseDown);
+  }
+  if (arrayMain[row+1] && arrayMain[row+1][col]) {
+    arrayMain[row+1][col].addEventListener("mousedown", handleMouseDown);
+  }
+  if (arrayMain[row-1] && arrayMain[row-1][col]) {
+    arrayMain[row-1][col].addEventListener("mousedown", handleMouseDown);
+  }
+
+
 }
